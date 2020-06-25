bebb040c8e369636823fa6c630c6453389e5cc5f
Dai Nguyen <ngtrdai290197@gmail.com>
2020-06-25 23:25:34 +0700

    (feat): added plugins and highlight code

--- scully.about-me-scully.config.ts
+++ scully.about-me-scully.config.ts
@@ -1,14 +1,39 @@
-import { ScullyConfig } from '@scullyio/scully';
-export const config: ScullyConfig = {
-  projectRoot: "./src",
-  projectName: "about-me-scully",
-  outDir: './dist/static',
-  routes: {
-    '/blog/:slug': {
-      type: 'contentFolder',
-      slug: {
-        folder: "./blog"
-      }
-    },
+import { ScullyConfig, RouteTypes, setPluginConfig } from '@scullyio/scully'
+import { getHttp404Plugin } from '@gammastream/scully-plugin-http404';
+import { MinifyHtml } from 'scully-plugin-minify-html';
+import { HljsHtml } from './plugins/hljs';
+
+
+const PageNotFoundPlugin = getHttp404Plugin();
+const minifyHtmlOptions = {
+  minifyOptions: {
+    removeComments: false
   }
 };
+setPluginConfig(MinifyHtml, minifyHtmlOptions);
+setPluginConfig('md', { enableSyntaxHighlighting: true });
+
+
+const postRenderers = [MinifyHtml, PageNotFoundPlugin]
+
+
+export const config: ScullyConfig = {
+  projectRoot: './src',
+  projectName: 'about-me-scully',
+  outDir: './dist/static',
+  appPort: 8080,
+  staticport: 8000,
+  defaultPostRenderers: postRenderers,
+  routes: {
+    '/blog/:slug': {
+      type: RouteTypes.contentFolder,
+      slug: {
+        folder: './blog',
+      },
+      postRenderers: [HljsHtml]
+    },
+    '/404': {
+      type: RouteTypes.default
+    }
+  },
+}
