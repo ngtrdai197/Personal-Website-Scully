2ffb6b31442cdaa2d9315840cf33603f18dd3c58
Nguyen Dai <ngtrdai290197@gmail.com>
2020-09-14 01:16:54 +0700

    enable purge css

--- configs/tailwind.config.js
+++ configs/tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   prefix: '',
   separator: ':',
   purge: {
-    enabled: false, // set true to enable => purge css
+    enabled: true, // set true to enable => purge css
     content: ['./src/**/*.html'],
   },
   theme: {
