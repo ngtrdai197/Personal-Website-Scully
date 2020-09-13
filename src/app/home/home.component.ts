9675391b3f7e1b018a9899270289c1c2ead2f5c6
Nguyen Dai <ngtrdai290197@gmail.com>
2020-09-14 01:36:31 +0700

    update url photo

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core'
 import { ScullyRoutesService } from '@scullyio/ng-lib'
-import { map, tap, shareReplay } from 'rxjs/operators'
+import { map } from 'rxjs/operators'
 
-import { SeoService, ISeo } from '@/app/core/services'
+import { SeoService } from '@/app/core/services'
 import { environment as env } from '@/environments/environment'
 
 @Component({
@@ -26,7 +26,7 @@ export class HomeComponent implements OnInit {
       title: 'Dai Nguyen',
       route: `${env.baseUrl}`,
       url: `${env.baseUrl}`,
-      image: `${env.baseUrl}/assets/avatar/jpeg`,
+      image: `${env.baseUrl}/assets/avatar.jpeg`,
       description:
         'I am a developer who is highly interested in TypeScript. My tech stack has been full-stack TS such as Angular, React with TypeScript and NestJS.',
     })
