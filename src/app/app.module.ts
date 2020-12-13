0fb54cb5f7bca3a780d19e250bf697d1d6df12b7
Nguyen Dai <ngtrdai290197@gmail.com>
2020-12-13 22:54:31 +0700

    update pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 import { ScullyLibModule } from '@scullyio/ng-lib'
+import { ServiceWorkerModule } from '@angular/service-worker'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { CoreModule } from '@/core/core.module'
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
 import { environment } from '../environments/environment'
 
 @NgModule({
   declarations: [AppComponent, PageNotFoundComponent],
-  imports: [BrowserModule, CoreModule, AppRoutingModule, ScullyLibModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
+  imports: [
+    BrowserModule,
+    CoreModule,
+    AppRoutingModule,
+    ScullyLibModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
