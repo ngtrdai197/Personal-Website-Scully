a71ee38c0a097aaab7aeff373e619af9679c7594
Nguyen Dai <ngtrdai290197@gmail.com>
2020-12-20 21:11:29 +0700

    chore(): update web url of companies infomation

--- src/app/about-me/components/experiences/experiences.component.ts
+++ src/app/about-me/components/experiences/experiences.component.ts
@@ -6,6 +6,7 @@ interface IExperience {
   company: string
   position: string
   tech: string
+  url: string
 }
 
 @Component({
@@ -18,7 +19,8 @@ export class ExperiencesComponent implements OnInit {
     {
       time: 'December 2020 - Present',
       title: 'Software Development Engineer',
-      company: '@B&K SOLUTION',
+      company: '@BnK SOLUTION',
+      url: 'https://bnksolution.com/vi_VN/',
       position: 'Fullstack web developer',
       tech:
         'Technologies: ReactJs, NestJs framework (NodeJs), Typescipt, PostgresQL, MySQL (TypeORM), Docker, ...',
@@ -27,6 +29,7 @@ export class ExperiencesComponent implements OnInit {
       time: 'September 2020 - November 2020',
       title: 'Software Development Engineer',
       company: '@FAVIE-TECH',
+      url:'https://favie.tech/',
       position: 'Fullstack web developer',
       tech:
         'Technologies: Angular, NestJs framework (NodeJs), GraphQL, Typescipt, PostgresQL (TypeORM), Docker, ...',
@@ -35,6 +38,7 @@ export class ExperiencesComponent implements OnInit {
       time: 'November 2019 - November 2020',
       title: 'Software Development Engineer',
       company: '@ANT-TECH',
+      url: 'https://ant-tech.eu/',
       position: 'Fullstack web developer',
       tech:
         'Technologies: Angular, NestJs framework (NodeJs), GraphQL, Typescipt, MongoDB, Docker, ...',
@@ -43,6 +47,7 @@ export class ExperiencesComponent implements OnInit {
       time: 'February 2019 - October 2019',
       title: 'Software Development Engineer',
       company: '@JANETO',
+      url: 'https://janeto.com/vi/',
       position: 'Fullstack web developer',
       tech: 'Technologies: Angular, NodeJs, Typescipt, MongoDB.',
     },
