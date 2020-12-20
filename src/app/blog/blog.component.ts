cdcb71bf0a7818f16563c1db3ffc8f49dd416623
Nguyen Dai <ngtrdai290197@gmail.com>
2020-12-20 15:13:33 +0700

    chore(): update data to improvement SEO

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core'
+import { ScullyRoutesService } from '@scullyio/ng-lib'
+import { shareReplay, tap } from 'rxjs/operators'
+
+import { ISeo, SeoService } from '@/core/services'
+import { environment as env } from '@/environments/environment'
+
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
@@ -7,6 +13,29 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core'
   encapsulation: ViewEncapsulation.Emulated,
 })
 export class BlogComponent implements OnInit {
-  constructor() {}
-  ngOnInit() {}
+  constructor(
+    private readonly scullyRoutesService: ScullyRoutesService,
+    private readonly seoService: SeoService,
+  ) {}
+  ngOnInit() {
+    this.scullyRoutesService
+      .getCurrent()
+      .pipe(
+        tap((blog: ISeo) => {
+          this.seoService.update(this.getDataSeo(blog))
+        }),
+        shareReplay(1),
+      )
+      .subscribe()
+  }
+
+  private getDataSeo(data: ISeo): ISeo {
+    return data?.title
+      ? {
+          ...data,
+          url: `${env.baseUrl}${data.route}`,
+          image: `${env.baseUrl}/${data.image}`,
+        }
+      : this.seoService.baseSeo
+  }
 }
